c621ba8a84621d3682e8a5710128a1a94ec60d27
Joe Dai <joe.dai@orionhealth.com>
2018-05-24 15:16:43 +1200

    first commit

---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Joe Dai on 4/8/18.
+//  Copyright © 2018 Joe Dai. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArry = [Item]()
+    let filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(filePath!)
+        loadItems();
+
+        
+//        if let storedList = defaults.stringArray(forKey: "ToDoList") {
+//            itemArry = storedList
+//        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK: - Table view source
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = itemArry[indexPath.row].title
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArry.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let item = itemArry[indexPath.row]
+        item.done = !item.done
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        cell?.accessoryType = cell?.accessoryType == .checkmark ? .none : .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        saveItems()
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new todoey item", message: "", preferredStyle: .alert)
+        
+        var textField = UITextField()
+        
+        let action = UIAlertAction(title: "Add new item", style: .default, handler: { (action) in
+            
+            let item = Item(context: self.context)
+            item.title = textField.text!
+            
+            self.itemArry.append(item)
+
+            self.saveItems()
+            
+            self.tableView.reloadData()
+        })
+            
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Item Name"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems(){
+        do{
+            try self.context.save()
+        }catch{
+            print("Error while saving array \(error)")
+        }
+    }
+    
+    func loadItems(){
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do{
+            itemArry = try self.context.fetch(request)
+        }catch{
+            print("Error while saving array \(error)")
+        }
+    }
+    
+}
+
